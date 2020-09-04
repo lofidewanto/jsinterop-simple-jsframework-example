2c32d674f6759b5a62828b1001d485e768a151e4
lofidewanto <lofidewanto@gmail.com>
2020-09-04 18:56:53 +0200

    add Calculator and CalculatorService

---
+++ src/main/java/com/github/lofi/client/CalculatorService.java
@@ -0,0 +1,24 @@
+package com.github.lofi.client;
+
+import java.util.logging.Logger;
+
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsType;
+
+@JsType(namespace = JsPackage.GLOBAL, name = "CalculatorService")
+public class CalculatorService {
+	
+	private static Logger logger = Logger.getLogger(CalculatorService.class.getName());
+	
+	public String calculatePrice(int x, int y) {
+        logger.info("CalculatorService: calculatePrice...");
+        
+        Calculator calculator = new Calculator();
+        calculator.x = x;
+        calculator.y = y;
+        final int sum = calculator.sum();
+
+        return "Price: " + sum;
+    }
+
+}
